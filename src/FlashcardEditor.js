8a54287e52a286aa3c858097c6ba1a85b9860c61
tonymoo90 <tonymoo90@gmail.com>
2024-09-13 12:25:08 -0500

    Update FlashcardEditor.js

--- src/FlashcardEditor.js
+++ src/FlashcardEditor.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 function FlashcardEditor({ flashcards, setFlashcards }) {
   const handleInputChange = (index, field, value) => {
