9c3b05b0fb61e64ce4563614ec5d5ed33d3b24bc
tonymoo90 <tonymoo90@gmail.com>
2024-09-14 10:26:25 -0500

    Revert "Update server.js"
    This reverts commit b6451805ed16b81e20ad8e02ffd807d8f1698302.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5001;
 
 // CORS setup to allow requests from your Netlify app
 const corsOptions = {
-  origin: '*', // Your Netlify URL
+  origin: 'https://extraordinary-hamster-30d45e.netlify.app', // Your Netlify URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
